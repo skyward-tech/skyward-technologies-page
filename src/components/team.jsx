a49f1b5051b901150e45d0711b1288e7d099b70d
Marco Antônio <mfontalvoantunes@hotmail.com>
2024-04-03 22:11:45 -0300

    images, styles and data

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -6,15 +6,12 @@ export const Team = (props) => {
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <p>The team that makes everything happen.</p>
         </div>
         <div id="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                <div key={`${d.name}-${i}`} className="col-md-4 col-sm-6 team">
                   <div className="thumbnail">
                     {" "}
                     <img src={d.img} alt="..." className="team-img" />
