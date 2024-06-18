0fe5e5013fd174abbf0ba3a2a258886cd2c6d664
Marco <mfontalvoantunes@gmail.com>
2024-06-18 11:35:52 -0300

    commented

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -37,21 +37,21 @@ export const Navigation = (props) => {
                 About
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="#services" className="page-scroll">
                 Services
               </a>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <a href="#portfolio" className="page-scroll">
                 Gallery
               </a>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <a href="#testimonials" className="page-scroll">
                 Testimonials
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#team" className="page-scroll">
                 Team
